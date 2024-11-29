c3a37b301dc28e540dec08c874818d6cb102584a
EnnaLea <soleariaeve@gmail.com>
2024-11-29 17:39:23 +0100

    implement running the program non stop

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import selectorlib
 import smtplib, ssl
 import os
+import time
 
 URL = "https://programmer100.pythonanywhere.com/tours"
 HEADERS = {
@@ -48,13 +49,14 @@ def send_email(message):
 
 
 if __name__ == "__main__":
-    scraped = scrape(URL)
-    extracted = extract(scraped)
-    print(extracted)
+    while True:
+        scraped = scrape(URL)
+        extracted = extract(scraped)
+        print(extracted)
 
-    content = read(extracted)
-    if extracted != "No upcoming tours":
-        if extracted not in content:
-            store(extracted)
-            send_email(message="Hey, new event was found!")
-
+        content = read(extracted)
+        if extracted != "No upcoming tours":
+            if extracted not in content:
+                store(extracted)
+                send_email(message="Hey, new event was found!")
+        time.sleep(2)
